88fc501bddeb2daa2aa748c46670d4fd57aa3908
chris dixon <chrisdixon161@googlemail.com>
2023-11-20 09:43:47 +0000

    removed unused line from async and defer lesson

--- 01.Javascript-Basics/02.Async & defer/script1.js
+++ 01.Javascript-Basics/02.Async & defer/script1.js
@@ -1,3 +1,2 @@
-document.addEventListener()
-document.getElementById('title').innerText = 'Javascript'
-document.getElementById('title').style.color = 'red'
+document.getElementById("title").innerText = "Javascript";
+document.getElementById("title").style.color = "red";
